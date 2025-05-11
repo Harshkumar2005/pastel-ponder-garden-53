2ed3ad57417c1f5a0fd29c16e2692f24220ffdfe
Harshkumar2005 <73623311+Harshkumar2005@users.noreply.github.com>
2025-05-11 14:56:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,41 @@ import Index from "./pages/Index";
 import QuoteDetail from "./pages/QuoteDetail";
 import NotFound from "./pages/NotFound";
 import BookmarkedQuotes from "./pages/BookmarkedQuotes";
+// Import the MobileNavbar component
 import MobileNavbar from "@/components/MobileNavbar";
 // Import the useIsMobile hook if you still need to conditionally render it based on screen size
 import { useIsMobile } from "@/hooks/use-mobile";
 
+
 const queryClient = new QueryClient();
 
-const App = () => (
-   const isMobile = useIsMobile();
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/quote/:id" element={<QuoteDetail />} />
-          <Route path="/bookmarks" element={<BookmarkedQuotes />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+const App = () => {
+  // If you want to conditionally render MobileNavbar based on screen size *within* App.tsx:
+  const isMobile = useIsMobile();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/quote/:id" element={<QuoteDetail />} />
+            <Route path="/bookmarks" element={<BookmarkedQuotes />} />
+            {/* Catch-all route for 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+
+          {/* Render the MobileNavbar here */}
+          {/* You can add the isMobile check back if you only want it in the DOM on mobile */}
         {isMobile && <MobileNavbar />}
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+          
+
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
